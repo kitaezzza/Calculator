76cdf835dbb37700f355dca2584b4fb0c2accb31
kitaezzza <kwakwawork@gmail.com>
2023-03-07 16:08:31 +0400

    что-то поменялось

--- kakuka/MainWindow.xaml.cs
+++ kakuka/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
 
 namespace kakuka
 {
@@ -23,11 +24,42 @@ namespace kakuka
         public MainWindow()
         {
             InitializeComponent();
+            
         }
 
         private void btn_result_Click(object sender, RoutedEventArgs e)
         {
-
+            double tb_one = Convert.ToDouble(tb_first_num.Text);
+            double tb_two = Convert.ToDouble(tb_second_num.Text);
+            if (rb_add.IsChecked.Value)
+            {
+                string result = Convert.ToString(tb_one + tb_two);
+                MessageBox.Show(result);
+            }
+            else if (rb_sub.IsChecked.Value)
+            {
+                string result = Convert.ToString(tb_one - tb_two);
+                MessageBox.Show(result);
+            }
+            else if (rb_multi.IsChecked.Value)
+            {
+                string result = Convert.ToString(tb_one * tb_two);
+                MessageBox.Show(result);
+            }
+            else if (rb_div.IsChecked.Value)
+            {
+                while (tb_two == 0)
+                {
+                    MessageBox.Show("Измените значение второго числа.", "Упс!");
+                    break;
+                }
+                string result = Convert.ToString(tb_one / tb_two);
+                MessageBox.Show(result);
+            }
+            else
+            {
+                MessageBox.Show("Выберите оператор.", "Упс!");
+            }
         }
     }
 }
